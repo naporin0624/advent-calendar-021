b26f7cddadf50ec3699a8332b8ed5074ceff3541
napori0624 <naporin0624@gmail.com>
2019-12-21 16:36:52 +0900

    :+1: use validator

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,7 +1,10 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id!: number;
+
+    @Column('varchar')
+    name: string;
 }
